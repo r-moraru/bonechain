d4c2c363fa0ba6e36eb635e950beb1ba4f53f241
Radu Moraru <77592590+r-moraru@users.noreply.github.com>
2023-06-06 15:47:05 +0300

    Delete blockchain.py

--- blockchain.py
+++
@@ -1,8 +0,0 @@
-from block import Block
-
-class Blockchain:
-    def __init__(self):
-        self._blocks = [self.create_genesis()]
-
-    def create_genesis(self) -> Block:
-        Block("", [])
